1524b43f4d8806a282d2bceaf6cf7480e128bd0f
limavit <limavit@gmail.com>
2023-08-18 18:28:24 -0300

    tratamento da resposta

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Course } from '../model/course';
+import { CoursesService } from './../services/courses.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -7,11 +10,13 @@ import { Course } from '../model/course';
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
-  courses: Course[] = [
-    { _id: '1', name: 'Angular', category: 'front-end'}
-  ];
+
+  courses: Observable<Course[]>;
   displayedColumns = ['name','category'];
-  constructor(){
+  //coursesService: CoursesService;
+  constructor(private coursesService: CoursesService){
+    //this.coursesService = new CoursesService();
+    this.courses = this.coursesService.list();
 
   }
   ngOnInit(): void{
